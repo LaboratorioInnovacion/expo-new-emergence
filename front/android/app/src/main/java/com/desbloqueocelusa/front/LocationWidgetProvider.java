1c64e872c96c1979a268aae1e8f0b55294415044
psiconervio <psiconervio@gmai.com>
2025-06-11 12:38:10 -0300

    asdsad

---
+++ front/android/app/src/main/java/com/desbloqueocelusa/front/LocationWidgetProvider.java
@@ -0,0 +1,25 @@
+package com.desbloqueocelusa.front;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViews;
+
+public class LocationWidgetProvider extends AppWidgetProvider {
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        for (int appWidgetId : appWidgetIds) {
+            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget_layout);
+
+            Intent intent = new Intent(context, LocationService.class);
+            PendingIntent pendingIntent = PendingIntent.getForegroundService(
+                context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE
+            );
+            views.setOnClickPendingIntent(R.id.widget_button, pendingIntent);
+
+            appWidgetManager.updateAppWidget(appWidgetId, views);
+        }
+    }
+}
